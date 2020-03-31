279f6cd01a34733daf9125613d5fd45ef955ab9f
Anton Selifanov <Tatoxer@gmail.com>
2020-03-31 18:39:06 +0300

    initial

---
+++ Linux Handbook/Handbook.py
@@ -0,0 +1,25 @@
+
+print("""1.Linux commands""")
+section = str(input("Choose\n"))
+
+if section == "1":
+    f = open("commands.txt", "r")
+    commands = f.read()
+    f.close()
+
+    commands = commands.split(";")
+    clear_list = []
+
+    for i in commands:
+        d = ""
+        for e in i:
+            if e != "\n":
+                d += e
+        clear_list.append(d)
+
+    clear_list.sort()
+    for i in clear_list:
+        print(i + "\n")
+
+else:
+    print("Goodby")
