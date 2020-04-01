31d728c57627543ccc44cb1edea09bd2bcf8acc7
Anton Selifanov <Tatoxer@gmail.com>
2020-04-01 13:03:35 +0300

    added new git section

--- Linux Handbook/Handbook.py
+++ Linux Handbook/Handbook.py
@@ -1,5 +1,5 @@
-def show_section(file, mode):
-    f = open(file, r)
+def show_section(file):
+    f = open(file, "r")
     commands = f.read()
     f.close()
 
@@ -12,14 +12,20 @@ def show_section(file, mode):
             if e != "\n":
                 d.lower()
                 d += e
+
         clear_list.append(d)
+
 
     clear_list.sort()
     for i in clear_list:
         print(i + "\n")
 
-    else:
-        print("Goodby")
+
+def read_file(file):
+    f = open(file, "r")
+    commands = f.read()
+    f.close()
+    print(commands)
 
 print("""1.Linux commands
 2.Git""")
@@ -28,9 +34,17 @@ section = str(input("Choose\n"))
 if section == "1":
     file = "commands.txt"
     r = "r"
-    show_section(file, "r")
+    show_section(file)
 
 elif section == "2":
-    file = "GIt.txt"
-    r = "r"
-    show_section(file, "r")
+    print("""1.Git install
+2.Git commands""" + "\n")
+    git_section = str(input("Choose section:\n"))
+
+    if git_section == "1":
+        file = "Git_install.txt"
+        read_file(file)
+
+    elif git_section == "2":
+        file = "Git_commnads.txt"
+        show_section(file)
